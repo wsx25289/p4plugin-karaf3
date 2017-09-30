c08bdaa6b49e51a0d52b1344eee6501a0b731329
wsx25289 <10200860@zte.com.cn>
2017-09-30 10:49:33 +0800

    update

--- adapter/netconf-adapter/impl/src/main/java/org/opendaylight/p4plugin/netconf/adapter/impl/DeviceInterfaceDataOperator.java
+++ adapter/netconf-adapter/impl/src/main/java/org/opendaylight/p4plugin/netconf/adapter/impl/DeviceInterfaceDataOperator.java
@@ -93,8 +93,8 @@ public class DeviceInterfaceDataOperator {
         builder.setGrpcServerIp(ip);
         builder.setGrpcServerPort(port);
         builder.setDeviceId(deviceId);
-        builder.setRuntimeFile("/home/opendaylight/odl/p4src/switch.proto.txt");
-        builder.setConfigFile(null);
+        builder.setRuntimeFile("/home/opendaylight/odl/p4src/simple_router_action_profile.proto.txt");
+        builder.setConfigFile("/home/opendaylight/odl/p4src/simple_router_action_profile.json");
         return builder.build();
     }
 
