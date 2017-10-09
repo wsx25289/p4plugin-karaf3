3f3faf51572a5dec780d8298a44c997a93afb97d
wsx25289 <10200860@zte.com.cn>
2017-10-09 19:34:04 +0800

    update

--- core/impl/src/main/java/org/opendaylight/p4plugin/core/impl/DeviceServiceProvider.java
+++ core/impl/src/main/java/org/opendaylight/p4plugin/core/impl/DeviceServiceProvider.java
@@ -30,8 +30,8 @@ public class DeviceServiceProvider implements P4pluginCoreDeviceService {
         Preconditions.checkArgument(input != null, "Add node RPC input is null.");
         AddNodeOutputBuilder builder = new AddNodeOutputBuilder();
         String nodeId = input.getNodeId();
-        String ip = input.getIp().getValue();
-        Integer port = input.getPort().getValue();
+        String ip = input.getGrpcServerIp().getValue();
+        Integer port = input.getGrpcServerPort().getValue();
         Long deviceId = input.getDeviceId().longValue();
         String runtimeFile = input.getRuntimeFile();
         String configFile = input.getConfigFile();
